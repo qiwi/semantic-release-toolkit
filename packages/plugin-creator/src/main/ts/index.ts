81cac7d743b9859733b6d9b992f1af26d9c54abd
Anton Golub <golub.anton@gmail.com>
2020-12-21 22:55:18 +0300

    chore(plugin-creator): add basic ifaces

--- packages/plugin-creator/src/main/ts/index.ts
+++ packages/plugin-creator/src/main/ts/index.ts
@@ -1,1 +1,27 @@
+import {Plugin, PluginFactory, ReleaseStep, ReleaseType} from './interface'
+import {Context} from "semantic-release";
+
 export const foo = 'bar'
+
+export * from './interface'
+
+const releaseSteps: Array<ReleaseStep> = [
+  'verifyConditions',
+  'analyzeCommits',
+  'verifyRelease',
+  'generateNotes',
+  'prepare',
+  'publish',
+  'success',
+  'fail'
+]
+
+export const createPlugin: PluginFactory = (releaseHandler) =>
+  releaseSteps.reduce<Plugin>((m, v) => {
+    m[v] = (pluginOptions: Record<any, any>, ctx: Context) => {
+      return releaseHandler(pluginOptions, ctx, v)
+    }
+
+    return m
+  }, {})
+
