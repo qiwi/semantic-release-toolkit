a423c14e950794e40bac376731b06adb18325aa6
Anton Golub <golub.anton@gmail.com>
2021-01-05 19:30:01 +0400

    test: normalize cwd path

--- packages/plugin-creator/src/test/ts/assets/file.ts
+++ packages/plugin-creator/src/test/ts/assets/file.ts
@@ -6,7 +6,7 @@ import {
   readdirSync,
   writeFileSync,
 } from 'fs'
-import { join } from 'path'
+import { isAbsolute,join, normalize } from 'path'
 
 // Deep copy a directory.
 export const copyDirectory = (source: string, target: string): void => {
@@ -55,4 +55,23 @@ export const createNewTestingFiles = (folders: string[], cwd: string): void => {
   folders.forEach((fld) => {
     writeFileSync(`${cwd}/${fld}test.txt`, fld)
   })
+}
+
+/**
+ * Normalize and make a path absolute, optionally using a custom CWD.
+ * Trims any trailing slashes from the path.
+ *
+ * @param {string} path The path to normalize and make absolute.
+ * @param {string} cwd=process.cwd() The CWD to prepend to the path to make it absolute.
+ * @returns {string} The absolute and normalized path.
+ *
+ * @internal
+ */
+export const cleanPath = (path: string, cwd = process.cwd()): string => {
+  // Checks.
+  // check(path, "path: path");
+  // check(cwd, "cwd: absolute");
+
+  // Normalize, absolutify, and trim trailing slashes from the path.
+  return normalize(isAbsolute(path) ? path : join(cwd, path)).replace(/[/\\]+$/, "");
 }
