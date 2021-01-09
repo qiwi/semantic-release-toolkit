51b119c64c2c50073121731934f618e3a20fa794
Anton Golub <golub.anton@gmail.com>
2021-01-09 17:57:30 +0300

    chore(metabranch): add fetch method

--- packages/metabranch/src/test/ts/index.ts
+++ packages/metabranch/src/test/ts/index.ts
@@ -1,18 +1,40 @@
 import { fetch, TSyncOptions } from '../../main/ts'
+import {copyDirectory, gitCommitAll, gitInit, gitInitOrigin, gitPush} from "@qiwi/semrel-testing-suite"
+import path from 'path'
+import tempy from 'tempy'
+import fs from 'fs'
+
+const fixtures = path.resolve(__dirname, '../fixtures')
 
 describe('metabranch', () => {
   describe('fetch()', () => {
+    const initTempRepo = (fixture = `${fixtures}/basicPackage/`): { cwd: string, repo: string } => {
+      const cwd = gitInit()
+      copyDirectory(fixture, cwd)
+      gitCommitAll(cwd, 'feat: initial commit')
+      const repo = gitInitOrigin(cwd)
+      gitPush(cwd)
+
+      return {
+        cwd,
+        repo
+      }
+    }
+
+
     it('clones files from remote to target dir', async () => {
+      const to = path.join(tempy.directory(), )
+      const { repo } = initTempRepo()
       const opts: TSyncOptions = {
         branch: 'gh-pages',
-        from: [],
-        to: '/foo',
-        repo: 'https://github.com/qiwi/semantic-release-toolkit.git'
+        from: '.',
+        to,
+        repo
       }
 
       await fetch(opts)
 
-      expect(fetch).toBe('bar')
+      expect(fs.readFileSync(path.join(to, 'package.json'), {encoding: 'utf8'})).toBe('bar')
     })
   })
 })
