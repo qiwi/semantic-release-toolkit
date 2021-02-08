502b68bcf331bb22afd5408cf36251fc87486ec8
Anton Golub <mailbox@antongolub.ru>
2021-02-08 21:20:53 +0300

    refactor(common): replace custom git root finder

--- packages/common/src/main/ts/git/init.ts
+++ packages/common/src/main/ts/git/init.ts
@@ -1,7 +1,4 @@
-import { Extends } from '@qiwi/substrate'
-import findUp, { Match } from 'find-up'
-import fs from 'fs'
-import path from 'path'
+import { gitRoot } from '@antongolub/git-root'
 import tempy from 'tempy'
 
 import { effect } from '../misc'
@@ -12,41 +9,13 @@ export interface IGitInit {
   sync?: boolean
 }
 
-export const gitFindUp = <S>(
-  cwd?: string,
-  sync?: S,
-): Extends<S, boolean, Match, Promise<Match>> => {
-  const exec = sync ? findUp.sync : findUp
-
-  return exec(
-    (directory) => {
-      const gitDir = path.join(directory, '.git')
-
-      return effect(exec.exists(gitDir), (exists) => {
-        if (!exists) {
-          return
-        }
-
-        const isDirectory = fs.lstatSync(gitDir).isDirectory()
-        if (isDirectory) {
-          return directory
-        }
-
-        const gitRef = fs.readFileSync(gitDir, { encoding: 'utf-8' })
-        const match = /^gitdir: (.*)\.git\s*$/.exec(gitRef)
-
-        return match ? match[1] : undefined
-      })
-    },
-    { type: 'directory', cwd },
-  ) as Extends<S, boolean, Match, Promise<Match>>
-}
+export { gitRoot }
 
 export const gitInit = <T extends IGitInit>({
   cwd = tempy.directory(),
   sync,
 }: T): TGitResult<T> =>
-  effect(gitFindUp(cwd, sync), (parentGitDir) => {
+  effect(gitRoot(cwd, sync), (parentGitDir) => {
     if (parentGitDir) {
       throw new Error(
         `${cwd} belongs to repo ${parentGitDir as string} already`,
