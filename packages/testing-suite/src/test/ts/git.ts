55447171882761f21cb0d236fae62134471af428
Anton Golub <mailbox@antongolub.ru>
2021-03-22 11:29:11 +0300

    feat(testing-suite): add enhanced fakeRepo builder

--- packages/testing-suite/src/test/ts/git.ts
+++ packages/testing-suite/src/test/ts/git.ts
@@ -6,16 +6,17 @@ import {
   gitAdd,
   gitBranch,
   gitCheckout,
+  gitClone,
   gitCommit,
   gitCommitAll,
-  // gitCommitAll,
   gitConfigGet,
+  gitCreateFakeRepo,
   gitGetTagHash,
   gitGetTags,
-  // gitInit,
   gitInitOrigin,
   gitInitTestingRepo,
   gitPush,
+  gitPushFakeCommits,
   gitTag,
 } from '../../main/ts'
 
@@ -23,6 +24,70 @@ const fixtures = resolve(__dirname, '../fixtures')
 const sync = true
 
 describe('testing suite', () => {
+  describe('gitCreateFakeRepo()', () => {
+    it('creates new fake repo', () => {
+      const { cwd, url, commits } = gitCreateFakeRepo({
+        sync,
+        commits: [
+          {
+            message: 'chore: initial commit',
+            from: `${fixtures}/basicPackage/`,
+            tag: 'foobar',
+            branch: 'foo',
+          },
+        ],
+      })
+      expect(cwd).toEqual(expect.any(String))
+      expect(
+        execa.sync('git', ['rev-parse', 'HEAD'], { cwd: cwd }).stdout,
+      ).toEqual(commits[0])
+
+      const _cwd = gitClone({ sync, url })
+
+      execa.sync('git', ['fetch', '--all'], { cwd: _cwd })
+      expect(
+        execa.sync('git', ['rev-parse', 'remotes/origin/foo'], { cwd: _cwd })
+          .stdout,
+      ).toEqual(commits[0])
+      expect(gitGetTags({ cwd, hash: commits[0], sync })).toEqual(['foobar'])
+    })
+  })
+  describe('gitPushFakeCommits()', () => {
+    it('adds commit to repo', () => {
+      const { cwd, url } = gitCreateFakeRepo({
+        sync,
+        commits: [],
+      })
+      const { commits } = gitPushFakeCommits({
+        cwd,
+        sync,
+        commits: [
+          {
+            message: 'feat: foo bar baz',
+            from: `${fixtures}/foo/`,
+            branch: 'foo',
+          },
+          {
+            message: 'feat: initial commit',
+            from: `${fixtures}/basicPackage/`,
+          },
+        ],
+      })
+
+      const _cwd = gitClone({ sync, url })
+      const _commits = execa.sync('git', ['log', '-10', '--format=format:%H'], {
+        cwd: _cwd,
+      }).stdout.split('\n')
+
+      expect(commits).toEqual(_commits.reverse())
+      expect(
+        execa.sync('git', ['rev-parse', 'remotes/origin/master'], {
+          cwd: _cwd,
+        }).stdout,
+      ).toEqual(commits[1])
+    })
+  })
+
   describe('gitInit()', () => {
     it('creates temp git dir, and returns its path', () => {
       const branch = 'foobarbaz'
