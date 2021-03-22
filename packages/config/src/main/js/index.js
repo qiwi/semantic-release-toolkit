c2bbc97e4e265e839e034671bf629210ae99db45
Anton Golub <mailbox@antongolub.ru>
2021-03-22 17:33:45 +0300

    feat(config): release refactoring as patche

--- packages/config/src/main/js/index.js
+++ packages/config/src/main/js/index.js
@@ -1,7 +1,19 @@
 module.exports = {
   branch: 'master',
   plugins: [
-    '@semantic-release/commit-analyzer',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          {type: 'docs', release: 'patch'},
+          {type: 'refactor', release: 'patch'},
+        ],
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES']
+        }
+      }
+    ],
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     '@qiwi/semantic-release-gh-pages-plugin',
