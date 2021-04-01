034c4c752a93dc9d1f3617806c4d950193288dca
Anton Golub <mailbox@antongolub.ru>
2021-04-01 19:26:55 +0300

    feat(plugin-creator): provide dual export for pkg

--- packages/plugin-creator/jest.config.js
+++ packages/plugin-creator/jest.config.js
@@ -1,1 +1,7 @@
-module.exports = {...require('../infra/jest.config.json')}
+import { readFileSync } from 'fs'
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
+
+const _dirname = dirname(fileURLToPath(import.meta.url))
+
+export default JSON.parse(readFileSync(resolve(_dirname, '../infra/jest.config.json'), { encoding: 'utf8' }))
