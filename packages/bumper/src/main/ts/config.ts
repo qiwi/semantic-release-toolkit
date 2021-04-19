baed8a952a1a2c5a00acbd2909a6e6e08f5d3d49
Anton Golub <mailbox@antongolub.ru>
2021-04-19 20:35:23 +0300

    test(bumper): test negative glob cases

--- packages/bumper/src/main/ts/config.ts
+++ packages/bumper/src/main/ts/config.ts
@@ -1,9 +1,9 @@
-import { sync as globby } from 'globby'
 import { readFileSync as read } from 'fs'
+
+import { sync as globby } from 'globby'
+
 import {
-  TBumperDirective,
-  TBumperConfig,
-  TBumperRules, TPackageName,
+  TPackageName,
 } from './interface'
 
 const asArray = <T>(value: T): T extends any[] ? T : T[] => (Array.isArray(value) ? value: [value]) as T extends any[] ? T : T[]
@@ -14,8 +14,8 @@ export const readPackagesNamesFromGlobs = (globs: string[], cwd: string): string
   globby(globs.map(p => p.replace(/\/package\.json$/, '') + '/package.json'), { cwd, absolute: true, onlyFiles: true })
     .map(file => JSON.parse(read(file, { encoding: 'utf8' })).name)
 
-export const resolvePackageNames = (targets: TPackageName | TPackageName[]) => {
-  asArray(targets)
+export const resolvePackageNames = (targets: TPackageName | TPackageName[], cwd: string): TPackageName[] => {
+  const {pkgs, globs} = asArray(targets)
     .reduce((memo, val) => {
       if (globlikePattern.test(val)) {
         memo.globs.push(val)
@@ -25,24 +25,6 @@ export const resolvePackageNames = (targets: TPackageName | TPackageName[]) => {
 
       return memo
     }, {pkgs: [] as string[], globs: [] as string[]})
-}
 
-
-
-export const resolveBumperConfig = (config: TBumperConfig, cwd: string = process.cwd()): TBumperRules => {
-  const directives: TBumperDirective[] = asArray(config)
-  const rules = directives.reduce((memo, { include }) => {
-    asArray(include).forEach(val => {
-      if (globlikePattern.test(val)) {
-        memo.globs.push(val)
-      } else {
-        memo.pkgs.push(val)
-      }
-    })
-
-    return memo
-  }, {pkgs: [] as string[], globs: [] as string[]})
-
-
-  return rules as any
+  return [...pkgs, ...readPackagesNamesFromGlobs(globs, cwd)]
 }
--- packages/bumper/src/test/ts/config.ts
+++ packages/bumper/src/test/ts/config.ts
@@ -1,18 +1,44 @@
-import { readPackagesNamesFromGlobs } from '../../main/ts'
 import { resolve } from 'path'
+
+import {
+  readPackagesNamesFromGlobs,
+  resolvePackageNames,
+} from '../../main/ts'
 
 const fixtures = resolve(__dirname, '../fixtures')
 
 describe('bumper/config', () => {
-  describe('readPackagesNamesFromGlobs', () => {
+  describe('readPackagesNamesFromGlobs()', () => {
     it('extracts name from package.json files', () => {
       expect(readPackagesNamesFromGlobs([
-        "packages/*",
-        "more-packages/*"
+        'packages/*',
+        'more-packages/*',
+        '!*/qux',
     ], resolve(fixtures, 'basicMonorepo'))).toEqual([
         'pkg-bar',
         'pkg-foo',
         'pkg-baz',
+      ])
+    })
+  })
+
+  describe('resolvePackageNames()', () => {
+    it('reads pkg names from globs', () => {
+      expect(resolvePackageNames([
+        'packages/*',
+        'more-packages/*',
+        '@qiwi/some-pkg'
+      ], resolve(fixtures, 'basicMonorepo'))).toEqual([
+        '@qiwi/some-pkg',
+        'pkg-bar',
+        'pkg-foo',
+        'pkg-baz',
+        'pkg-qux',
+      ])
+
+      expect(resolvePackageNames('packages/*', resolve(fixtures, 'basicMonorepo'))).toEqual([
+        'pkg-bar',
+        'pkg-foo',
       ])
     })
   })
