39579d58b3d3c7f26968501ddb588cec83da6cc3
Anton Golub <antongolub@antongolub.com>
2021-08-24 12:19:56 +0300

    refactor: migrate to @qiwi/esm

---
+++ packages/plugin-creator/src/test/cjs/index.js
@@ -0,0 +1,7 @@
+const {createPlugin} = require('@qiwi/semrel-plugin-creator') // eslint-disable-line
+
+describe('cjs', () => {
+  it('createPlugin', () => {
+    expect(createPlugin).toEqual(expect.any(Function))
+  })
+})
