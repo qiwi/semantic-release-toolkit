32d04a8aacfe524cd1568905c5da0485034b29ca
Demian <d.sergienko@qiwi.com>
2022-10-05 13:43:47 +0300

    fix: migrate to zx-bulk-release

--- packages/metabranch/.releaserc.cjs
+++ packages/metabranch/.releaserc.cjs
@@ -1,35 +1,5 @@
 module.exports = {
-  debug: true,
-  branch: 'master',
-  plugins: [
-    [
-      '@qiwi/semrel-metabranch',
-      {
-        publish: {
-          action: 'push',
-          branch: 'gh-pages',
-          from: './docs',
-          to: '.',
-          message: 'update meta ${nextRelease.gitTag}'
-        }
-      }
-    ],
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
-    '@semantic-release/npm',
-    [
-      '@semantic-release/github',
-      {
-        successComment: false,
-        failComment: false
-      }
-    ],
-    [
-      '@semantic-release/git',
-      {
-        message: 'chore(release): ${nextRelease.gitTag} [skip ci]\n\n${nextRelease.notes}'
-      }
-    ]
-  ]
+  cmd: 'yarn',
+  changelog: 'changelog',
+  ghPages: 'gh-pages docs',
 }
